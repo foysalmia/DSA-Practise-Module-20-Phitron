10deed72914927d9f01e2ab30a3056a999dbf557
Foysal Ahmed <mfoysal314@gmail.com>
2022-08-12 00:36:18 +0600

    1 done

--- 1/1.cpp
+++ 1/1.cpp
@@ -3,6 +3,18 @@
 using namespace std;
 
 int main(){
-    cout<<"Hello world !"<<endl;
+    int n,m;
+    cin>>n>>m;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    sort(arr,arr+n);
+
+    int minMax = arr[m-1];
+    int bigMax = arr[n-m];
+
+    cout<<minMax<<" "<<bigMax;
+
     return 0;
 }
